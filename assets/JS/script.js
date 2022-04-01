6d19cd6758185ecf2b19d53c037bae6dba2d3781
Priyam Carto <cpriyam90@gmail.com>
2022-04-01 12:14:54 -0400

    localstorage values created

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -9,7 +9,6 @@ currentDay.text(currentDate);
 //Create function to match past, present, and future to each hour 
 $(".description").each(function(){
     var now = moment().hour();
-    console.log(now);
     var currentHour = $(this).parent().attr("id");
     if (parseInt(currentHour)===now){
         $(this).addClass("present");
@@ -23,11 +22,22 @@ $(".description").each(function(){
 })
 
 //Create function to save tasks to each matching hour
-$(".saveBtn").each(function(){
-    var btn = $(this).parent().attr("id")
-    console.log(btn)
-    localStorage.setItem("description", JSON.stringify(description))
-
+$(".saveBtn").on("click", function(){
+    var time = $(this).parent().attr("id");
+    var text = $(this).siblings(".description").val();
+    localStorage.setItem(time, text);
 })
+
+//Get saved textarea description from localstorage for each hour
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+
 
 
